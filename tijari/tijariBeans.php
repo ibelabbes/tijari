e050facc9b68654f3f222709d2241a89b5d5f1c2
ibelabbes <35175093+ibelabbes@users.noreply.github.com>
2018-01-06 16:31:31 -0600

    Update tijariBeans.php

--- tijari/tijariBeans.php
+++ tijari/tijariBeans.php
@@ -1,9 +1,9 @@
 <?php
 
     function escapeJavascript($str){
-            return str_replace("\r\n", "<br>", str_replace("\"", "\\\"", str_replace("'", "\'",htmlspecialchars($str)))) ;	
-    } 
-	  
+            return str_replace("\r\n", "<br>", str_replace("\"", "\\\"", str_replace("'", "\'",htmlspecialchars($str)))) ;
+    }
+
     class Vector {
         /*
          * The number of valid components in this Vector object.
@@ -46,7 +46,7 @@
 
 
       /*
-       * Returns the current size of this Vector 
+       * Returns the current size of this Vector
        */
        public function size()
        {
@@ -110,11 +110,11 @@
 
       /*
        * Removes element at $index
-       **/ 
+       **/
 
-          public function removeAt($index) 
+          public function removeAt($index)
           {
-           if ($this->checkBound($index)) 
+           if ($this->checkBound($index))
                {
                      for ($i = $index; $i != $this->elementCount - 1; $i++)
                       {
@@ -124,7 +124,7 @@
            $this->elementCount = count($this->elementData);
 
            return true;
-           } 
+           }
            else {
                 return false;
                 }
@@ -132,11 +132,11 @@
 
               /*
        * Removes Card from Hand
-       **/ 
+       **/
 
-          public function removeCard($card) 
+          public function removeCard($card)
           {
-           if ($this->contains($card)) 
+           if ($this->contains($card))
                {
                      for ($i = 0; $i < $this->elementCount; $i++)
                       {
@@ -147,7 +147,7 @@
                                     }
 
                     return true;
-           } 
+           }
            else {
                 return false;
                 }
@@ -180,7 +180,7 @@
        }
 
         /*
-         *Removes the first element from the Vector 
+         *Removes the first element from the Vector
          */
 
         public function removeFirstElement()
@@ -195,8 +195,8 @@
         }
 
     }
-    
-    class Tourney {        
+
+    class Tourney {
         private $number   = 0;
         private $hostName = null;
         private $location = null;
@@ -204,106 +204,109 @@
         private $time     = null;
         private $games    = null;
 
-        function Tourney($hostName1, $location1, $date1, $time1)
+        //function Tourney($hostName1, $location1, $date1, $time1)
+        function __construct($hostName1, $location1, $date1, $time1)
         {
             $this->hostName = $hostName1;
             $this->location = $location1;
-            $this->date     = $date1;      
-            $this->time     = $time1; 
+            $this->date     = $date1;
+            $this->time     = $time1;
             $this->games    = new Vector;
-        }    
-        
-	function getNumber()
-	{
-            return $this->number;
-	}  
-        
-        function setNumber($n)
-	{
-            $this->number = $n;
-	} 
-        
-        function getHostName()
-	{
-            return $this->hostName;
-	}   
-        function getLocation()
-	{
-            return $this->location;
-	}
-        function getDate()
-	{
-           return $this->date;
-	}
-        function getTime()
-	{
-            return $this->time;
-	}    
-        
-        function getGames()
-	{
-            return $this->games;
-	}
-        
-        function setGames($games1)
-	{
-            $this->games = $games1;
-	}  
-        
-        function addGame($game1)
-	{
-            $this->games->add($game1);
-	}        
-    }  
-    
-    class Player {        
+        }
+
+		function getNumber()
+		{
+				return $this->number;
+		}
+
+		function setNumber($n)
+		{
+				$this->number = $n;
+		}
+
+		function getHostName()
+		{
+				return $this->hostName;
+		}
+		function getLocation()
+		{
+				return $this->location;
+		}
+		function getDate()
+		{
+			   return $this->date;
+		}
+		function getTime()
+		{
+				return $this->time;
+		}
+
+		function getGames()
+		{
+				return $this->games;
+		}
+
+		function setGames($games1)
+		{
+				$this->games = $games1;
+		}
+
+		function addGame($game1)
+		{
+				$this->games->add($game1);
+		}
+    }
+
+    class Player {
         private $firstName = null;
         private $lastName  = null;
         private $playerId  = 0;
 
-        function Player($playerId1, $firstName1, $lastName1)
+        //function Player($playerId1, $firstName1, $lastName1)
+        function __construct($playerId1, $firstName1, $lastName1)
         {
             $this->firstName = $firstName1;
             $this->lastName  = $lastName1;
-            $this->playerId  = $playerId1;      
-        }    
-           
+            $this->playerId  = $playerId1;
+        }
+
         function getFirstName()
-	{
-		return $this->firstName;
-	}
-        function getLastName()
-	{
-		return $this->lastName;
-	}     
-        function getPlayerId()
-	{
-		return $this->playerId;
-	}            
-    }  
-    
-    class Team {        
+		{
+			return $this->firstName;
+		}
+		function getLastName()
+		{
+			return $this->lastName;
+		}
+		function getPlayerId()
+		{
+			return $this->playerId;
+		}
+    }
+
+    class Team {
         private $player1 = null;
         private $player2 = null;
 
-        function Team($player1, $player2)
+        //function Team($player1, $player2)
+        function __construct($player1, $player2)
         {
             $this->player1 = $player1;
-            $this->player2 = $player2;      
-        }    
-        
-	function getPlayer1()
-	{
-            return $this->player1;
-	} 
-        
-	function getPlayer2()
-	{
-            return $this->player2;
-	}        
+            $this->player2 = $player2;
+        }
+
+		function getPlayer1()
+		{
+				return $this->player1;
+		}
+
+		function getPlayer2()
+		{
+				return $this->player2;
+		}
     }
-    
-    class Game {        
+
+    class Game {
         private $team1           = null;
         private $team2           = null;
         private $team1TotalScore = 0;
@@ -312,68 +315,69 @@
         private $tourneyId       = 0;
         private $gameNumber      = 0;
 
-        function Game($player11, $player12, $player21, $player22, $tourneyId1, $gameNumber1)
+        //function Game($player11, $player12, $player21, $player22, $tourneyId1, $gameNumber1)
+        function __construct($player11, $player12, $player21, $player22, $tourneyId1, $gameNumber1)
         {
             $this->team1       = new Team($player11, $player12);
-            $this->team2       = new Team($player21, $player22); 
+            $this->team2       = new Team($player21, $player22);
             $this->subGames    = new Vector;
             $this->tourneyId   = $tourneyId1;
-            $this->gameNumber  = $gameNumber1; 
-        }    
-        
-	function getTeam1()
-	{
-            return $this->team1;
-	} 
-        
-	function getTeam2()
-	{
-            return $this->team2;
-	}        
-        
-	function getTeam1TotalScore()
-	{
-            return $this->team1TotalScore;
-	}  
-        
-        function getTeam2TotalScore()
-	{
-            return $this->team2TotalScore;
-	}
-        
-	function setTeam1TotalScore($score1)
-	{
-            $this->team1TotalScore = $score1;
-	}  
-        
-        function setTeam2TotalScore($score1)
-	{
-            $this->team2TotalScore = $score1;
-	}        
-        
-        function getSubGames()
-	{
-            return $this->subGames;
-	}   
-        
-        function getGameNumber()
-	{
-            return $this->gameNumber;
-	}
-        
-        function addSubGame($subGame)
-	{
-            $this->subGames->add($subGame);
-	} 
-        
-        function getTourneyId()
-	{
-            return $this->tourneyId;
-	} 
-              
+            $this->gameNumber  = $gameNumber1;
+        }
+
+		function getTeam1()
+		{
+				return $this->team1;
+		}
+
+		function getTeam2()
+		{
+				return $this->team2;
+		}
+
+		function getTeam1TotalScore()
+		{
+				return $this->team1TotalScore;
+		}
+
+			function getTeam2TotalScore()
+		{
+				return $this->team2TotalScore;
+		}
+
+		function setTeam1TotalScore($score1)
+		{
+				$this->team1TotalScore = $score1;
+		}
+
+			function setTeam2TotalScore($score1)
+		{
+				$this->team2TotalScore = $score1;
+		}
+
+			function getSubGames()
+		{
+				return $this->subGames;
+		}
+
+			function getGameNumber()
+		{
+				return $this->gameNumber;
+		}
+
+			function addSubGame($subGame)
+		{
+				$this->subGames->add($subGame);
+		}
+
+			function getTourneyId()
+		{
+				return $this->tourneyId;
+		}
+
     }
-    
-    class SubGame {        
+
+    class SubGame {
         private $team1Score       = 0;
         private $team2Score       = 0;
         private $team1ScoreResult = null;
@@ -381,66 +385,67 @@
         private $subGameNumber    = 0;
         private $gameId           = 0;
 
-        function SubGame($subGameNumber1, $gameId1)
+        //function SubGame($subGameNumber1, $gameId1)
+        function __construct($subGameNumber1, $gameId1)
         {
-            $this->subGameNumber = $subGameNumber1; 
+            $this->subGameNumber = $subGameNumber1;
             $this->gameId        = $gameId1;
-        }    
-                
-	function getTeam1Score()
-	{
-            return $this->team1Score;
-	}
-        
-	function getTeam2Score()
-	{
-            return $this->team1Score;
-	}         
-        
-	function setTeam1Score($score)
-	{
-            $this->team1Score = $score;
-	}
-        
-	function setTeam2Score($score)
-	{
-            $this->team2Score = $score;
-	} 
-        
-        function getTeam1ScoreResult()
-	{
-            return $this->team1ScoreResult;
-	}
-        
-        function getTeam2ScoreResult()
-	{
-            return $this->team2ScoreResult;
-	}    
-        
-        function setTeam1ScoreResult($result1)
-	{
-            $this->team1ScoreResult = $result1;
-	}
-        
-        function setTeam2ScoreResult($result1)
-	{
-            $this->team2ScoreResult = $result1;
-	}       
-        
-        function getSubGames()
-	{
-            return $this->subGames;
-	}   
-        
-        function getSubGameNumber()
-	{
-            return $this->subGameNumber;
-	}
-        
-        function getGameId()
-	{
-            return $this->gameId;
-	}        
-    }    
+        }
+
+		function getTeam1Score()
+		{
+				return $this->team1Score;
+		}
+
+		function getTeam2Score()
+		{
+				return $this->team1Score;
+		}
+
+		function setTeam1Score($score)
+		{
+				$this->team1Score = $score;
+		}
+
+		function setTeam2Score($score)
+		{
+				$this->team2Score = $score;
+		}
+
+			function getTeam1ScoreResult()
+		{
+				return $this->team1ScoreResult;
+		}
+
+			function getTeam2ScoreResult()
+		{
+				return $this->team2ScoreResult;
+		}
+
+			function setTeam1ScoreResult($result1)
+		{
+				$this->team1ScoreResult = $result1;
+		}
+
+			function setTeam2ScoreResult($result1)
+		{
+				$this->team2ScoreResult = $result1;
+		}
+
+			function getSubGames()
+		{
+				return $this->subGames;
+		}
+
+			function getSubGameNumber()
+		{
+				return $this->subGameNumber;
+		}
+
+			function getGameId()
+		{
+				return $this->gameId;
+		}
+    }
 
 ?>
