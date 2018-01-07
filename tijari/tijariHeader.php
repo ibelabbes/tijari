3e99188c4458ec134a41bd6e258271168556ef51
ibelabbes <35175093+ibelabbes@users.noreply.github.com>
2018-01-06 23:06:18 -0600

    Update tijariHeader.php

--- tijari/tijariHeader.php
+++ tijari/tijariHeader.php
@@ -10,6 +10,6 @@
 	//production
 	//$file_path="http://www.mufts.com";
         //$file_path="http://chicago-tijari.rhcloud.com";
-        $file_path="tijari-tijari.1d35.starter-us-east-1.openshiftapps.com";
+        $file_path="http://tijari-tijari.1d35.starter-us-east-1.openshiftapps.com";
 
 ?>
